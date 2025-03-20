6a3851c342244b1bd1f9e8c8fa4a098d46789fc7
Camila Chica <camilachica1991@gmail.com>
2025-03-20 13:19:13 -0400

    Generating Random Values

--- app.py
+++ app.py
@@ -8,10 +8,32 @@
 #print(max(numbers))
 
 #Packages
-from ecommerce import shipping
+#from ecommerce import shipping
 #Other way to import multiple functions: from ecommerce.shipping import cal_shipping, cal_tax
 
-shipping.cal_shipping()
+#shipping.cal_shipping()
 
+#Generating Random Values
+import random
 
+for i in range(3):
+    print(random.random())
 
+for i in range(3):
+    print(random.randint(10, 20))
+
+members = ['John', 'Mary', 'Bob', 'Camila']
+leader = random.choice(members)
+print(leader)
+
+#Exercise
+
+import random
+class Dice:
+    def roll(self):
+       first =  random.randint(1, 6)
+       second = random.randint(1, 6)
+       return (first, second) #In python, we can also write without parenthesis and it takes it as a Tuple, like this: ""return first, second"
+
+dice = Dice()
+print(dice.roll())
