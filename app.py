8a3fc948fa10b7b30e2aff0a764128b54038ff84
Camila Chica <camilachica1991@gmail.com>
2025-03-20 12:46:17 -0400

    Exercise

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ lbs_to_kg(100)
 
 print(converters.kg_to_lbs(70))
 
+#Ercecise
+numbers = [10, 3, 6, 2]
+max = numbers[0]
+for number in numbers:
+    if number > max:
+        max = numbers
+print(max)
+
